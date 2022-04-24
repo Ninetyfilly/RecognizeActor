2a96c78f7e98358758cd1ff5f3fb26e48cb5a7e9
Luis Carballo <ninetyfilly975@gmail.com>
2022-04-24 16:58:29 -0500

    Primer commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Screen from './Views/screen';
+
+function App() {
+  return <Screen />;
+}
+
+export default App;
